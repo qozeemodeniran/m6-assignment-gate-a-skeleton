5ca69ca0fd61fb6826aec7bf5046017d86222e4d
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-09-21 15:49:16 +0100

    added variables

--- app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
+++ app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
@@ -3,7 +3,16 @@ package mooc.vandy.java4android.gate.logic;
 /**
  * This file defines the Gate class.
  */
-public class Gate {
-    // TODO -- Fill in your code here
+public class Gate
+{
+    // field
+    private int mSwing;
+
+    // Variables to hold values for, IN, OUT and CLOSE
+    public static final int OUT = -1;
+    public static final int IN = 1;
+    public static final int CLOSE = 0;
+
+    // Creating Getter and Setter methods
 
 }
